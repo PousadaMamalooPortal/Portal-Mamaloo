308c7886ad4f0cfdd9b15df8df4ce1a55ea56704
Alan Farias <aaof.alan@gmail.com>
2025-04-29 16:06:47 -0300

    feat: Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/Header.css'; 
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src="/src/assets/mamaloo-logo.png" alt="Mamaloo Pousada" className="logo" />
+      </div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#rooms">Quartos</a></li>
+          <li><a href="#attractions">Atrações</a></li>
+          <li><a href="#reviews">Avaliações</a></li>
+          <li><a href="#book">Reservar</a></li>
+        </ul>
+      </nav>
+    </header>
+
+  );
+};
+
+export default Header;
