4c458a1d89daf39843e18d9706cddbe4b26f780a
Alan Farias <aaof.alan@gmail.com>
2025-04-29 16:20:32 -0300

    feat: Main e App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+// Remover a importação do index.css caso tenha removido esse arquivo
+// import './index.css'; // Esta linha pode ser apagada se não houver mais necessidade do index.css
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
