e47461ab0622c622ecc7d8988ba94cddbf58f579
Alan Farias <aaof.alan@gmail.com>
2025-05-12 15:09:55 -0300

    refactor: Transformando o Footer em componente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import PaginaPrincipal from './components/paginaPrincipal';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <PaginaPrincipal/>
+      <Footer />
       <section id="home">
   
       </section>
--- frontend/src/components/paginaPrincipal.jsx
+++ frontend/src/components/paginaPrincipal.jsx
@@ -188,37 +188,6 @@ const PaginaPrincipal = () => {
   </div>
 </section>
 
-{/* Rodapé */}
-<footer className="rodape">
-<div className="info-contato">
-  <span>
-    <a href="https://www.instagram.com/mamaloopousada/" target="_blank" rel="noopener noreferrer">
-      <img src="/assets/icones/mamaloo-icone-instagram.png" alt="Instagram" />
-      @mamaloopousada
-    </a>
-  </span>
-  <span>
-    <a href="mailto:mamaloo@gmail.com">
-      <img src="/assets/icones/mamaloo-icone-email.png" alt="Email" />
-      mamaloo@gmail.com
-    </a>
-  </span>
-  <span>
-    <a href="https://api.whatsapp.com/send?phone=5582981815454" target="_blank">
-      <img src="/assets/icones/mamaloo-icone-whats.png" alt="Telefone" />
-      +55 (82) 98181-5454
-    </a>
-  </span>
-</div>
-
-  <div className="info-local">
-    <strong>Pousada Mamaloo</strong><br />
-    R. Dr. Waldemíro Alencar, 37, 126 - Jatiúca, Maceió - AL, 57035-860
-  </div>
-
-  <p className="desenvolvimento">Desenvolvido por: <br />2025 © Alunos IFAL</p>
-</footer>
-
     </div>
   );
 };
