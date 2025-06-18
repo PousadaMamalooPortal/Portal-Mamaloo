2be8f6cc90a58d6572a4cd568545e919d40b5ad6
Alan Farias <aaof.alan@gmail.com>
2025-06-18 18:08:50 -0300

    refactor: Ajuste do Vite para integração

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5185,
     allowedHosts: ['profbrenojac.com.br'], // Libera o domínio da VPS
+    proxy: {
+      // Quando sua aplicação React fizer uma requisição para '/api',
+      // o Vite vai encaminhá-la para 'http://localhost:8025'
+      '/api': {
+        target: 'http://devBackend-portalMamaloo:8025', // URL da sua API
+        changeOrigin: true, // Importante para lidar com cabeçalhos de host
+        // Se sua API **NÃO** espera o prefixo '/api' na URL,
+        // descomente a linha abaixo para removê-lo antes de proxyar:
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
