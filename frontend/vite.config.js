ac2fb2dc1366eb7c60715a95fabd1369dea38329
Alan Farias <aaof.alan@gmail.com>
2025-06-20 15:10:27 -0300

    refactor: Retirando proxy do Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,17 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5185,
-    allowedHosts: ['mamaloopousada.com.br'], // Libera o domínio da VPS
-    proxy: {
-      // Quando sua aplicação React fizer uma requisição para '/api',
-      // o Vite vai encaminhá-la para 'http://localhost:8025'
-      '/api': {
-        target: 'http://devBackend-portalMamaloo:8025', // URL da sua API
-        changeOrigin: true, // Importante para lidar com cabeçalhos de host
-        // Se sua API **NÃO** espera o prefixo '/api' na URL,
-        // descomente a linha abaixo para removê-lo antes de proxyar:
-        // rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+    allowedHosts: ['mamaloopousada.com.br'], 
+ 
   },
 })
