9e3e60103efc162bece04ed09cd04432380977f2
Alan Farias <aaof.alan@gmail.com>
2025-06-20 16:49:59 -0300

    refactor: Integração Atrações

--- frontend/src/components/Atracoes.jsx
+++ frontend/src/components/Atracoes.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/Atracoes.css';
+import { URL_API } from '../Api.js';
 
-const URL_API = 'https://dev-portal.mamaloopousada.com.br'; 
+
 function Atracoes() {
   const [atracoes, setAtracoes] = useState([]);
 
